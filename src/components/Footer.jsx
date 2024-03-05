9509b196b717ec27e97c1e4adaea45bc0362e832
Ameenah Jalil <ameenah_234@icloud.com>
2024-03-05 15:36:44 +0000

    Added more styling to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import linkedinLogo from "../assets/linkedin.svg"
+import githubLogo from "../assets/github.svg"
+import "../App.css"
 
 
 
 function Footer() {
     return (
+        <>
 
-<div>
+<footer>
         {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a> */}
         <a href="https://www.linkedin.com/in/ameenah-jalil/" target="_blank">
           <img src={linkedinLogo} className="logo react" alt="Linkedin logo" />
         </a>
-      </div>
+        <a href="https://github.com/AmeenahJalil" target="_blank">
+          <img src={githubLogo} className="logo github" alt="Github logo" />
+        </a>
+        <h4>© 2024 Ameenah Jalil. All Rights Reserved</h4>
+
+      </footer>
+      
+      </>
      
     );
   }
--- src/components/Hi.jsx
+++ src/components/Hi.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import selfie from "../assets/selfie.jpeg"
+import "../App.css"
 
 function Hi() {
   return (
     <>
    <p>Hi there! My name is Ameenah, I am a junior software developer.I have a background in finance, psychology and education. I have a new found passion for coding and love baking !</p>
-   <img src= {selfie} alt="A selfie of Ameenah"/>
+   <img className = "selfie" src= {selfie} alt="A selfie of Ameenah"/>
    </>
   );
 }
