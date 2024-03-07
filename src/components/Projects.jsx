6d5df52b497e5e035259d313820f7d5049329f5e
Ameenah Jalil <ameenah_234@icloud.com>
2024-03-07 19:01:04 +0000

    Added a description to the bookmard app

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,11 +7,20 @@ function Projects(){
         <>
         <h2 id="projects">Projects</h2>
         <h3>Bookmarkd</h3>
+        <div className = "flex-container">
+        
+        
+        
         <a className="bookmarkd screenshot" href="https://bookmarkd-ruddy.vercel.app/" target="_blank">
           <img src={bookmarkd} className="bookmarkdImage" alt="bookmarkd screenshot" />
         </a>
+        
+        <h4>Bookmarkd is an app where you can easily input your reading progress, find reccomendations using AI and create your own customised bookshelves. It is every book lovers dream app. </h4>
+        </div>
         <h3>Try the app <a href="https://bookmarkd-ruddy.vercel.app/" target="_blank">here</a></h3>
+        
         </>
+        
 
     )
 }
