4155a6e8a0abc26d4b5dd9155d05b90cc5807b89
Ameenah Jalil <ameenah_234@icloud.com>
2024-03-07 18:49:23 +0000

    Made the bookmarkd image smaller

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import bookmarkd from "../assets/bookmarkd.png"
+import "../App.css"
 
 function Projects(){
     return(
         <>
         <h2 id="projects">Projects</h2>
         <h3>Bookmarkd</h3>
-        <a href="https://bookmarkd-ruddy.vercel.app/" target="_blank">
-          <img src={bookmarkd} className="bookmarkd image" alt="bookmarkd screenshot" />
+        <a className="bookmarkd screenshot" href="https://bookmarkd-ruddy.vercel.app/" target="_blank">
+          <img src={bookmarkd} className="bookmarkdImage" alt="bookmarkd screenshot" />
         </a>
         <h3>Try the app <a href="https://bookmarkd-ruddy.vercel.app/" target="_blank">here</a></h3>
         </>
