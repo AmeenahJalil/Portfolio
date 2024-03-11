9bfc2c947b31f15149bd749d8afbca5945f7a6e4
Ameenah Jalil <ameenah_234@icloud.com>
2024-03-11 15:08:27 +0000

    Added input in the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,28 @@ function Contact (){
     <>
     <h2>Contact Me</h2>
     <h3>If you would like to get in touch with me to learn more about my projects, or for any other queries please do! I look forward to hearing from you!</h3>
-    <h2>Full Name</h2>
-    {/* <input>First and Last</input> */}
-    <h2>Email Address</h2>
+    <label>
+        Full Name: <input name="myInput" />
+      </label>
+      <hr/>
+      <label>
+      Email Address: <input name="myInput" />
+      </label>
+      <hr/>
+      <label>
+      Message:  <input name="myInput" />
+      </label>
+      <hr/>
+
+
+
+
+
+
+   
+    <h2></h2>
     {/* <input>email@domain.ltd</input> */}
-    <h2>Message</h2>
+    <h2></h2>
     {/* <input>Please enter your message here ...</input> */}
     <button>Submit</button>
     
