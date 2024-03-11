16ccc07c6dc5b648f8c921a1b3129dc41bb886b2
Ameenah Jalil <ameenah_234@icloud.com>
2024-03-11 14:18:56 +0000

    Added my second project onto the website

--- src/components/Hi.jsx
+++ src/components/Hi.jsx
@@ -5,9 +5,10 @@ import "../App.css"
 function Hi() {
   return (
     <>
+    <img className = "selfie" src= {selfie} alt="A selfie of Ameenah"/>
     <p>Hi there! My name is Ameenah, I am a junior software developer.I have a background in finance, psychology and education. I have a new found passion for coding and love baking !</p>
    
-   <img className = "selfie" src= {selfie} alt="A selfie of Ameenah"/>
+   
    </>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import bookmarkd from "../assets/bookmarkd.png"
+import BakingWebsite from "../assets/BakingWebsite.png"
 import "../App.css"
 
 function Projects(){
@@ -18,6 +19,23 @@ function Projects(){
         <h4>Bookmarkd is an app where you can easily input your reading progress, find reccomendations using AI and create your own customised bookshelves. It is every book lovers dream app. </h4>
         </div>
         <h3>Try the app <a href="https://bookmarkd-ruddy.vercel.app/" target="_blank">here</a></h3>
+
+        <h3>My baking website</h3>
+        <div className = "flex-container">
+        
+        
+        
+        <a className="baking website screenshot" href="https://bookmarkd-ruddy.vercel.app/" target="_blank">
+          <img src={BakingWebsite} className="bookmarkdImage" alt="Baking website screenshot" />
+        </a>
+        
+        <h4>I created this website which showcases all of the different biscuits, cakes and other sweet treats I have baked. This website was made using vanilla javascript. (It was before I knew what React was!) </h4>
+        </div>
+        <h3>Find your next recipe for your favourite sweet treat! <a href="https://bookmarkd-ruddy.vercel.app/" target="_blank">here</a></h3>
+        
+        
+
+    
         
         </>
         
